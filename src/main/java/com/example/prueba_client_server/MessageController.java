3a86db4ef876c1e5ca799c1c44ba1ee065da65ff
Daniel Bermudez <daniel.bermudez@kairosds.com>
2022-12-13 15:43:27 +0100

    chore: update springboot 3.0.0

--- src/main/java/com/example/prueba_client_server/MessageController.java
+++ src/main/java/com/example/prueba_client_server/MessageController.java
@@ -2,24 +2,31 @@ package com.example.prueba_client_server;
 
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+
 
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
-@RestController
+@Controller
 public class MessageController {
 
-    private WebClient webClient;
 
-    @GetMapping(value = "/articles")
+    private  final WebClient webClient;
+
+    public MessageController(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+
+    @GetMapping(value = "/articles", params = "grant_type=authorization_code")
     public String[] getArticles(
-            @RegisteredOAuth2AuthorizedClient("articles-client-authorization-code") OAuth2AuthorizedClient authorizedClient
+            @RegisteredOAuth2AuthorizedClient("messaging-client-authorization-code") OAuth2AuthorizedClient authorizedClient
     ) {
         return this.webClient
                 .get()
-                .uri("http://127.0.0.1:8090/messages")
+                .uri("http://localhost:8090/messages")
                 .attributes(oauth2AuthorizedClient(authorizedClient))
                 .retrieve()
                 .bodyToMono(String[].class)
