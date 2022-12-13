3f2279c4841a073ef9b8a2b16e6373be2758c255
devcostin <devcostin@gmail.com>
2022-12-13 13:34:06 +0100

    NUevo

---
+++ src/main/java/com/example/prueba_client_server/PruebaClientServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.prueba_client_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaClientServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaClientServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prueba_client_server/PruebaClientServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba_client_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaClientServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
